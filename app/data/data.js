15212b051a1975790c092dc04c46ab9de0ade34a
Emil Dimitrov <emil_132@abv.bg>
2017-10-02 09:24:37 +0300

    Added posts & users data

---
+++ app/data/data.js
@@ -0,0 +1,9 @@
+import { posts } from '../data/posts.data';
+import { users } from '../data/users.data';
+
+const data = {
+    posts: posts,
+    users: users,
+};
+
+export { data };
---
+++ app/data/posts.data.js
@@ -0,0 +1,64 @@
+import { requester } from '../utils/requester';
+
+const posts = (() => {
+    const LOCALSTORAGE_AUTH_KEY_NAME = 'daily-discovery-auth-key';
+
+    return {
+        getPost(id) {
+            return requester.get(`/api/posts/${id}`);
+        },
+
+        getAllPosts(pageNumber) {
+            if (typeof pageNumber === 'undefined') {
+                pageNumber = 1;
+            }
+
+            return requester.get(`/api/posts/page/${pageNumber}`);
+        },
+
+        publishPost(post) {
+            const body = {
+                title: post.title,
+                content: post.content,
+                imageUrl: post.imageUrl,
+                tags: post.tags
+            };
+
+            const category = post.category;
+
+            const headers = {
+                'daily-discovery-auth-key': localStorage.getItem(LOCALSTORAGE_AUTH_KEY_NAME)
+            };
+
+            return requester.post(`/api/posts/${category}`, body, headers);
+        },
+
+        postComment(comment) {
+            const body = {
+                id: comment.postId,
+                text: comment.text
+            };
+
+            const headers = {
+                'daily-discovery-auth-key': localStorage.getItem(LOCALSTORAGE_AUTH_KEY_NAME)
+            };
+
+            return requester.post(`/api/comments/${comment.postId}`, body, headers);
+        },
+
+        postReplyComment(comment) {
+            const body = {
+                id: comment.commentId,
+                text: comment.text
+            };
+
+            const headers = {
+                'daily-discovery-auth-key': localStorage.getItem(LOCALSTORAGE_AUTH_KEY_NAME)
+            };
+
+            return requester.post(`/api/comments/reply/${comment.commentId}`, body, headers);
+        },
+    };
+})();
+
+export { posts };
