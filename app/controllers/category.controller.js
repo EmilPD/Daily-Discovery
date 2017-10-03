df65b6f4064acaab6eb78c82aaa582a156d40aad
Emil Dimitrov <emil_132@abv.bg>
2017-10-03 10:43:56 +0300

    Added pagination to category

--- app/controllers/category.controller.js
+++ app/controllers/category.controller.js
@@ -1,18 +1,39 @@
 import { data } from '../data/data';
 import { templateLoader as tl} from '../utils/template-loader';
 import { handlebarsSubstr } from '../helpers/handlebars-substr';
+import { commonController } from '../controllers/common.controller';
 
 class CategoryController {
-    loadCategory(categoryName) {
+    loadCategory(categoryName, pageNumber) {
+        commonController.loadAll();
 
         Promise.all([
-            data.categories.getByName(categoryName),
+            data.categories.getByName(categoryName, pageNumber),
             tl.loadTemplate('category')
         ])
         .then(([posts, template]) => {
             if (posts.result !== null) {
                 $('#main').html(template({posts}));
             }
+        })
+        .then(() => {
+            data.posts.getCategoryPaginationInfo(categoryName)
+            .then((paginationInfo) => {
+                if (paginationInfo.postsCount === 'undefined') {
+                    commonController.loadPageNotFound();
+                    return;
+                }
+
+                $('#pagination').pagination({
+                    currentPage: pageNumber,
+                    items: paginationInfo.postsCount,
+                    itemsOnPage: paginationInfo.postsPerPage,
+                    prevText: '«',
+                    nextText: '»',
+                    hrefTextPrefix: '#/categories/' + categoryName + '/'
+                });
+            })
+            .catch(console.log);
         })
         .catch(console.log);
     }
