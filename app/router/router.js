d0b862e16b0792e2c6e4fbb90adadeec97ab28d8
Emil Dimitrov <emil_132@abv.bg>
2017-10-04 17:42:39 +0300

    Added more routes to navigo router

--- app/router/router.js
+++ app/router/router.js
@@ -1,4 +1,9 @@
+import { adminController } from '../controllers/admin.controller';
+import { usersController } from '../controllers/users.controller';
+import { postsController } from '../controllers/posts.controller';
 import { homeController } from '../controllers/home.controller';
+import { categoryController } from '../controllers/category.controller';
+import { commonController } from '../controllers/common.controller';
 
 const router = (() => {
     let navigo;
@@ -16,7 +21,34 @@ const router = (() => {
             'home/:pageNumber': (params) => {
                 homeController.loadHome(params.pageNumber);
             },
-            
+            'dashboard': () => {
+                adminController.loadDashboard();
+            },
+            'register': () => {
+                usersController.loadRegister();
+            },
+            'login': () => {
+                usersController.loadLogin();
+            },
+            'logout': () => {
+                usersController.loadLogout();
+            },
+            'publishpost': () => {
+                postsController.loadPublishPost();
+            },
+            'categories/:categoryName/': (params) => {
+                categoryController.loadCategory(params.categoryName);
+            },
+            'categories/:categoryName/:pageNumber': (params) => {
+                categoryController.loadCategory(params.categoryName, params.pageNumber);
+            },
+            'posts/:id': (params) => {
+                postsController.loadPost(params.id);
+            },
+        }).resolve();
+
+        navigo.notFound(() => {
+            commonController.loadPageNotFound();
         }).resolve();
         
     }
