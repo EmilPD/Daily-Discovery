cbc91e3a81271aed4902fc9f60a6650ecdb98669
Emil Dimitrov <emil_132@abv.bg>
2017-10-04 11:13:29 +0300

    Added pagination to home controller

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -1,13 +1,29 @@
 import { data } from '../data/data';
 import { templateLoader as tl} from '../utils/template-loader';
 import { handlebarsSubstr } from '../helpers/handlebars-substr';
+import { commonController } from '../controllers/common.controller';
 
 class HomeController {
     loadHome(pageNumber) {
+        commonController.loadAll();
         $('#home-link').addClass('active');
 
-        Promise.all([tl.loadTemplate('home')])
-            .then(([template]) =>  $('#main').html(template))
+        Promise.all([data.posts.getAllPosts(pageNumber), tl.loadTemplate('home')])
+            .then(([result, template]) =>  $('#main').html(template(result)))
+            .then(() => {
+                data.posts.getHomePaginationInfo()
+                .then((paginationInfo) => {
+
+                    $('#pagination').pagination({
+                        currentPage: pageNumber,
+                        items: paginationInfo.postsCount,
+                        itemsOnPage: paginationInfo.postsPerPage,
+                        prevText: '«',
+                        nextText: '»',
+                        hrefTextPrefix: '#/home/'
+                    });
+                });
+            })
             .catch(console.log);
     }
 }
