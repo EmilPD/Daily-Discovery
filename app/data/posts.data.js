acf9dee265202b43c5933b13b7a23f6bcbad9333
Emil Dimitrov <emil_132@abv.bg>
2017-10-05 17:42:46 +0300

    Added edit & delete posts to post data

--- app/data/posts.data.js
+++ app/data/posts.data.js
@@ -8,6 +8,14 @@ const posts = (() => {
             return requester.get(`/api/posts/${id}`);
         },
 
+        getHomePaginationInfo() {
+            return requester.get(`/api/posts/paginationInfo`);
+        },
+
+        getCategoryPaginationInfo(categoryName) {
+            return requester.get(`/api/posts/paginationInfo/${categoryName}`);
+        },
+
         getAllPosts(pageNumber) {
             if (typeof pageNumber === 'undefined') {
                 pageNumber = 1;
@@ -16,12 +24,28 @@ const posts = (() => {
             return requester.get(`/api/posts/page/${pageNumber}`);
         },
 
+        getNumberOfPosts(count) {
+            return requester.get(`/api/posts/count/${count}`);
+        },
+
+        getSearchPosts(searchTerm) {
+            return requester.get(`/api/posts/search/${searchTerm}`);
+        },
+
+        getRelatedPosts(categoryId, postId) {
+            return requester.get(`/api/posts/related/${categoryId}/${postId}`);
+        },
+
         getRecentPosts(count) {
             return requester.get(`/api/posts/recent/${count}`);
         },
 
         getRecentComments() {
             return requester.get(`/api/posts/comments`);
+        },
+
+        getPopularPosts() {
+            return requester.get(`/api/posts/popular`);
         },
 
         publishPost(post) {
@@ -39,6 +63,32 @@ const posts = (() => {
             };
 
             return requester.post(`/api/posts/${category}`, body, headers);
+        },
+
+        editPost(post, id) {
+            const body = {
+                title: post.title,
+                category: post.category,
+                content: post.content,
+                imageUrl: post.imageUrl,
+                tags: post.tags
+            };
+
+            const headers = {
+                'daily-discovery-auth-key': localStorage.getItem(LOCALSTORAGE_AUTH_KEY_NAME)
+            };
+
+            return requester.post(`/api/posts/edit/${id}`, body, headers);
+        },
+
+        deletePost(postId) {
+            const body = {};
+
+            const headers = {
+                'daily-discovery-auth-key': localStorage.getItem(LOCALSTORAGE_AUTH_KEY_NAME)
+            };
+
+            return requester.post(`/api/posts/delete/${postId}`, body, headers);
         },
 
         postComment(comment) {
